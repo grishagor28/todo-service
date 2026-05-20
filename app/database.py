d068b6f6b61a772f4306aab03ce11e841959c8f6
grishagor28 <dapsvach135@gmail.com>
2026-05-20 14:29:36 +0300

    feat: switch from SQLite to PostgreSQL

--- app/database.py
+++ app/database.py
@@ -1,9 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./todo.db"
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5432/todo_db"
+)
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 
